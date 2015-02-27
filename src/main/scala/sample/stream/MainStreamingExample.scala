7417bbd17a283b4471804a44c8858ebe668872ff
al333z <alessandro.zoffoli@gmail.com>
2015-02-27 21:41:45 +0100

    Minor changes

--- src/main/scala/sample/stream/MainStreamingExample.scala
+++ src/main/scala/sample/stream/MainStreamingExample.scala
@@ -20,7 +20,8 @@ object MainStreamingExample extends App {
   implicit val materializer = ActorFlowMaterializer()(system)
 
   // create a TwitterStreamClient, that pubbish on the event bus
-  val twitterStream = new TwitterStreamClient(system).init
+  val twitterStream = new TwitterStreamClient(system)
+  twitterStream.init // start getting status
 
   // create a Source, with an actor that listen items from the event bus
   val tweets: Source[Status] = Source(Props[StatusPublisherActor])
